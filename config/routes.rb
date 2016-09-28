bc94fb0bd87a0c50709a00824492c6cbe5965e51
Catherine French <catherine.french@me.com>
2016-09-28 19:22:58 -0400

    Formatting changes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
  resources :users do
    resources :voyages
  end
+ resources :reservations do
+ end
+
+
 
  get 'login' => 'user_sessions#new', :as => :login
  post 'logout' => 'user_sessions#destroy', :as => :logout
