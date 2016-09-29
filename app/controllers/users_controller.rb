2df518e654f9a0f03cd3397a791e6733cfc72d9d
Catherine French <catherine.french@me.com>
2016-09-29 17:49:49 -0400

    Updates user#show to display reservations but only in ActiveRecord form so far.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @users = User.all
     @user = User.find(params[:id])
-
+    @reservation = @user.reservations
     # find(params[:id])
     # @voyages = User.voyages
   end
