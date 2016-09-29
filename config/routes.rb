883072cff1bfddebc54435ad252747447a495e34
Catherine French <catherine.french@me.com>
2016-09-28 21:21:35 -0400

    Adding routes.rb with nested reservations for voyages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,13 @@ Rails.application.routes.draw do
 
  root :to => 'voyages#index'
  resources :user_sessions
- resources :users  
+ resources :users
 
 
  resources :voyages do
    resources :reservations
  end
-
-
-
-
+ 
  get 'login' => 'user_sessions#new', :as => :login
  post 'logout' => 'user_sessions#destroy', :as => :logout
 
