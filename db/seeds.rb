e6370e09bf417dd14e98ef64ac683c673c4a5910
Your Name <arssonist@yahoo.com>
2016-09-30 18:34:52 -0400

    commit to push- no changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user1 = User.create!(name: "joe", email: "joe@joe.com", password: "1234", password_confirmation: "1234")
+user2 = User.create!(name: "tom", email: "tom@tom.com", password: "1234", password_confirmation: "1234")
+user3 = User.create!(name: "john", email: "john@john.com", password: "1234", password_confirmation: "1234")
+
+voyage1 = Voyage.create!(title: "Voyage1", location: "Toronto", start_time: DateTime.parse("2016-09-30 11:11:00"), capacity: 1, captain: user1)
+voyage2 = Voyage.create!(title: "Voyage2", location: "Toronto", start_time: DateTime.parse("2016-09-30 11:11:00"), capacity: 1, captain: user2)
+voyage3 = Voyage.create!(title: "Voyage3", location: "Toronto", start_time: DateTime.parse("2016-09-30 11:11:00"), capacity: 1, captain: user3)
+
+reservation1 = Reservation.create!(passenger: user1, voyage_id: voyage2.id)
+reservation2 = Reservation.create!(passenger: user2, voyage_id: voyage3.id )
+reservation3 = Reservation.create!(passenger: user2, voyage_id: voyage2.id )
