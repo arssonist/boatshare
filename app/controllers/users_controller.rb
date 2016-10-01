691e33e5a0f3ac05470f3ba9996154b3ec737762
Your Name <arssonist@yahoo.com>
2016-10-01 15:31:44 -0400

    fixed nav bar and other bugs. Commented up loop to display all passengers due to bug. Will fix with assistance.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reservation = @user.reservations
-      @voyage = @voyage.passengers
+      #@voyage = @voyage.passengers
     # find(params[:id])
     # @voyages = User.voyages
   end
