e0036f81b6ae093a84ed7e4a8e81d91aaca3907f
Catherine French <catherine.french@me.com>
2016-10-01 21:49:38 -0400

    Fixes bug where user does not remain logged in after registration by updating user.save redirect_to in users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-    redirect_to(:users, notice: 'User was successfully created') #redirects user to index
+    redirect_to(:voyages, notice: 'User was successfully created') #redirects user to index
     end
 
     respond_to do |format|
