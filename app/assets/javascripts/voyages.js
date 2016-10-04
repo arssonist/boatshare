7fd41f5d9f3ca944e3efbfd581d19bb4232b8783
Your Name <arssonist@yahoo.com>
2016-10-03 20:07:30 -0400

    made modal window working, with close button and clickable background

--- app/assets/javascripts/voyages.js
+++ app/assets/javascripts/voyages.js
@@ -2,5 +2,17 @@ $(document).ready(function(){
   $('#voyage-button').click(function(event){
     event.preventDefault();
        $('.modal').fadeIn('slow');
-     })
+    })
+  })
+$(document).ready(function(){
+  $('.close').click(function(event){
+    event.preventDefault();
+    $('.modal').fadeOut('slow');
+  })
+})
+$(document).ready(function(){
+    $('.modal').click(function(event){
+      event.preventDefault();
+      $('.modal').fadeOut('slow');
+  })
 })
