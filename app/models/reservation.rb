cb048d190e77a5f3f8e08550a996281a43d19f4f
Your Name <arssonist@yahoo.com>
2016-10-05 00:03:21 -0400

    cleaned up seeds, attempted to add images and temp commented out validations for now

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,16 +6,16 @@ class Reservation < ApplicationRecord
 
   validates  :passenger_id, presence: true
   # validate :passenger_not_captain?
-    def passenger_not_captain? #custom validation method to make sure the passenger is not the captain
-     if  self.voyage.captain_id == passenger_id
-      errors.add(:passenger_id, message: "You can't book a place on your own trip! Please try again.")
-     end
-  end
+  #   def passenger_not_captain? #custom validation method to make sure the passenger is not the captain
+  #    if  self.voyage.captain_id == passenger_id
+  #     errors.add(:passenger_id, message: "You can't book a place on your own trip! Please try again.")
+  #    end
+  # end
 
     # validate :capacity_full? #validates capcity: if there is no space on boat then rollback reservation and display custom error.
-      def capacity_full
-        self.voyage.capacity - self.voyage.reservations.count
-      end
+      # def capacity_full
+      #   self.voyage.capacity - self.voyage.reservations.count
+      # end
 
 
       #   <= 0
