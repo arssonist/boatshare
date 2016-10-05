522bf41b19ad3c58d9f9df9d56c382283b68970b
Your Name <arssonist@yahoo.com>
2016-10-05 13:16:51 -0400

    worked on jquery on modal window. Tested form id to implement ajax on form

--- app/assets/javascripts/voyages.js
+++ app/assets/javascripts/voyages.js
@@ -1,13 +1,23 @@
+// to make modal window fade in
 $(document).ready(function(){
   $('#voyage-button').click(function(event){
-    event.preventDefault();
        $('.modal').fadeIn('slow');
     })
+    // to allow the close button to close modal window
   $('.close').click(function(event){
     event.preventDefault();
     $('.modal').fadeOut('slow');
   })
+  // to stop close of window on completion to update reservation
+  // NEXT: Flash notice and change number
+  $("input[type='submit'][name='commit']").click(function(event){
+    event.preventDefault();
+    // $(this).
+  })
 
+
+  $
+// makes window fade out on background click
   $('.modal').click(function(event){
       $(this).fadeOut('slow');
   })
