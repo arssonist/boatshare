a1d65d0f4dee46427e49bac5e0adc42460849afb
Catherine French <catherine.french@me.com>
2016-10-13 17:18:13 -0400

    Adds actionmailer.delivery_method smtp for default settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "https://hidden-beach-16599.herokuapp.com" }
 
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
    :port           => ENV['MAILGUN_SMTP_PORT'],
    :address        => ENV['MAILGUN_SMTP_SERVER'],
