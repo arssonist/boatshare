26e09a435c494574549c5f7e93787c5b811911db
Your Name <arssonist@yahoo.com>
2016-10-14 13:06:05 -0400

    fix map zoom

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,19 +3,18 @@ $(document).ready(function(){
         $('#map div').toggleClass('scrolloff');
 
       });
-    // remove pointer-events: none or scrolloff, making it clickables
 
-      $('#map').on('click',function(){
-        $(this).removeClass('scrolloff');
-      });
+      // $('#map').on('click',function(){
+      //   $(this).removeClass('scrolloff');
+      // });
 
-      $('#map div').mouseleave(function () {              // becuase the mouse up doesn't work...
-        alert('test')
-          $('#map div').addClass('scrolloff');            // set the pointer events to none when mouse leaves the map area
-        $('#map').on("mousedown",function(){        // when mouse down, set the mouse events free
-
-          $('#map div').removeClass('scrolloff');
-        });                                // or you can do it on some other event
-      });
-
+      // $('#map div').mouseleave(function () {              // becuase the mouse up doesn't work...
+  //     //   alert('test')
+  //         $('#map div').addClass('scrolloff');            // set the pointer events to none when mouse leaves the map area
+  //       $('#map').on("mousedown",function(){        // when mouse down, set the mouse events free
+  //
+  //         $('#map div').removeClass('scrolloff');
+  //       });                                // or you can do it on some other event
+  //     });
+  //
   });
